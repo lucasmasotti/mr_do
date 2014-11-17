160eeba676beb193a5c8359b76541c2b3ea4ce53
Lucas Rogério Masotti <lucasmasotti@gmail.com>
2014-11-17 21:04:50 -0200

    Nova versão
    Mr. Do agora colide corretamente com o ninho, fantasmas e frutas.
    Adicionadas o esqueleto das funções colidiu_fantasma e colidiu_fruta, que ainda precisam ser implementadas.
    O programa agora funciona com cores

--- Mr_Do.c
+++ Mr_Do.c
@@ -4,15 +4,46 @@
 #define NUMERO_LINHAS 23
 #define NUMERO_COLUNAS 60
 
-void imprime_matriz(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]);
-struct coordenada posicao_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]);
-void move_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS], struct coordenada coordenada_inicial, char direcao);
-void gotoxy(int x, int y);
 
-struct coordenada{
+// Structs
+typedef struct
+{
 	int linha;
 	int coluna;
-};
+} Coordenada;
+
+typedef struct
+{
+	char name[40];
+	int score; 
+} Highscore;
+
+typedef struct
+{
+	int score;
+	int ghosts;
+} GameState;
+
+
+void imprime_matriz(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]);
+Coordenada posicao_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]);
+void move_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS], Coordenada coordenada_inicial, char direcao);
+void gotoxy(int x, int y);
+
+char novo_char(char char_antigo); 
+
+void nova_cor(char caractere_impresso);
+
+//TO DO
+void colidiu_fantasma();
+void colidiu_fruta();
+
+
+
+
+
+// VariÃ¡veis globais
+char caractere_mr_do_passou = 'v';
 
 int main(){
 	char matriz[NUMERO_LINHAS][NUMERO_COLUNAS] =
@@ -42,6 +73,17 @@ int main(){
 		"vvvvvvvvvvvvvvvvvvvvvvvdvvvvvvvvvvivvvvvvvvvvvvvvvvppppppppp"
     };
     char entrada_usuario;
+	int opcao_usuario;
+
+	
+	
+	printf("\n1 - Novo jogo");
+	printf("\n2 - Continuar");
+	printf("\n3 - High Scores");
+	printf("\n4 - Sair");
+	printf("\n\nDigite a opcao desejada\n-> ");
+	scanf("%d", &opcao_usuario);
+	
 
     imprime_matriz(matriz);
     while(1){
@@ -49,6 +91,7 @@ int main(){
             entrada_usuario = getch();
             move_mr_do(matriz, posicao_mr_do(matriz), entrada_usuario);
             imprime_matriz(matriz);
+			Sleep(10);
         }
     }
 
@@ -56,12 +99,12 @@ int main(){
 	return 0;
 }
 
-/// Funções:
+/// FunÃ§Ãµes:
 
 /// Imprime a mtriz mat, substituindo os caracters pelos respectivos caracteres que o jogador deve visualizar
 void imprime_matriz(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]){
 	/*
-	Caracter 	| Significado 			| Código ASCII
+	Caracter 	| Significado 			| CÃ³digo ASCII
 		p	 			parede					176
 		i				fantasma				153
 		f				fruta					229
@@ -76,6 +119,7 @@ void imprime_matriz(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]){
     gotoxy(0, 0);
 	for(linha = 0; linha < NUMERO_LINHAS; linha++){
 		for(coluna = 0; coluna < NUMERO_COLUNAS; coluna++){
+			nova_cor(mat[linha][coluna]);
 			switch(mat[linha][coluna]){
 				case 'p':
 					printf("%c", 176);
@@ -104,10 +148,10 @@ void imprime_matriz(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]){
 	}
 }
 
-/// Recebe uma struct com a coordenada inicial do Mr. Do e uma posicao ('d', 'e', 'c', 'b'). Move o Mr. Do para a posicao indicada
-void move_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS], struct coordenada coordenada_inicial, char direcao){
+/// Recebe uma struct com a Coordenada inicial do Mr. Do e uma posicao ('d', 'e', 'c', 'b'). Move o Mr. Do para a posicao indicada
+void move_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS], Coordenada coordenada_inicial, char direcao){
 	/*
-	Direções:
+	DireÃ§Ãµes:
 	w - cima
 	s - baixo
 	d - direita
@@ -115,29 +159,34 @@ void move_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS], struct coordenada coord
 	*/
 	int linha_inicial = coordenada_inicial.linha;
 	int coluna_inicial = coordenada_inicial.coluna;
+	char char_substituido = novo_char(caractere_mr_do_passou);
 	switch(direcao){
 		case 'w':
 		    if((linha_inicial-1) >= 0){
-                mat[linha_inicial][coluna_inicial] = 'v';
+                mat[linha_inicial][coluna_inicial] = char_substituido;
+				caractere_mr_do_passou = mat[linha_inicial-1][coluna_inicial];
                 mat[linha_inicial-1][coluna_inicial] = 'd';
 		    }
 			break;
 		case 's':
 		    if((linha_inicial+1) <= 22){
-                mat[linha_inicial][coluna_inicial] = 'v';
+                mat[linha_inicial][coluna_inicial] = char_substituido;
+				caractere_mr_do_passou = mat[linha_inicial+1][coluna_inicial];
                 mat[linha_inicial+1][coluna_inicial] = 'd';
 		    }
 			break;
 		case 'd':
 		    if((coluna_inicial+1) <= 59){
-                mat[linha_inicial][coluna_inicial] = 'v';
+                mat[linha_inicial][coluna_inicial] = char_substituido;
+				caractere_mr_do_passou = mat[linha_inicial][coluna_inicial+1];
                 mat[linha_inicial][coluna_inicial+1] = 'd';
 		    }
 
 			break;
 		case 'a':
 		    if((coluna_inicial-1) >= 0){
-                mat[linha_inicial][coluna_inicial] = 'v';
+                mat[linha_inicial][coluna_inicial] = char_substituido;
+				caractere_mr_do_passou = mat[linha_inicial][coluna_inicial-1];
                 mat[linha_inicial][coluna_inicial-1] = 'd';
 		    }
 			break;
@@ -146,22 +195,96 @@ void move_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS], struct coordenada coord
 
 }
 
-/// Retorna uma struct do tipo coordenada (int linha, int coluna), que contém os índices da posição que ocupada pelo caracter'd'
-struct coordenada posicao_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]){
+/// Retorna uma struct do tipo coordenada (int linha, int coluna), que contÃ©m os Ã­ndices da posiÃ§Ã£o que ocupada pelo caracter'd'
+Coordenada posicao_mr_do(char mat[NUMERO_LINHAS][NUMERO_COLUNAS]){
 	int i = 0;
 	int j = 0;
 	for(i = 0; i < NUMERO_LINHAS; i++){
 		for(j = 0; j < NUMERO_COLUNAS; j++){
 			if(mat[i][j] == 'd'){
-			struct coordenada coordenada_mr_do = {i, j};
+			Coordenada coordenada_mr_do = {i, j};
 			return coordenada_mr_do;
 			}
 		}
 	}
 }
 
+
 void gotoxy(int x, int y){
     COORD coord = {0, 0};
     coord.X = x; coord.Y = y; // X and Y coordinates
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+/// Returna um novo character baseado no character antigo, alÃ©m de chamar os eventos corretos para a colisÃ£o com fantasma ou com frutas
+char novo_char(char char_antigo){
+	switch(char_antigo){
+		case 'v':
+			return 'v';
+			break;
+		case 'p':
+			return 'v';
+			break;
+		case 'n':
+			return 'n';
+			break;
+		case 'i':
+			colidiu_fantasma();
+			return 'v';
+			break;
+		case 'f':
+			colidiu_fruta();
+			return 'v';
+			break;
+		default:
+			return 'v';
+			break;
+	}
+}
+
+/// Muda a cor do terminal de acordo com o caractere que serÃ¡ impresso
+void nova_cor(char caractere_impresso){
+	/*
+		Cores:
+		1 - Azul escuro			 9 - Azul claro
+		2 - Verde				10 - Verde neon
+		3 - Azul				11 - Azul
+		4 -	Vermelho escuro		12 - Vermelho
+		5 -	Roxo				13 - Rosa
+		6 -	Dourado				14 - Amarelo
+		7 -	Branco				15 - Branco ?
+		8 - Cinza	
+	*/
+
+	int cor = 15;
+	// NecessÃ¡rio para alterar a cor do console
+	HANDLE h_console;
+	h_console = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	switch(caractere_impresso){
+		case 'p':
+			cor = 2;
+			break;
+		case 'f':
+			cor = 12;
+			break;
+		case 'd':
+			cor = 14;
+			break;
+		case 'i':
+			cor = 15;
+			break;
+		case 'n':
+			cor = 8;
+			break;
+	}
+	SetConsoleTextAttribute(h_console, cor);
+}
+
+void colidiu_fantasma(){
+
+}
+
+void colidiu_fruta(){
+
 }
